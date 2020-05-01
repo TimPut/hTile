39cf86aa56b7b09b0d7234ce7b2059b98af57331
timput <tim@timput.com>
2020-05-01 11:41:41 -0600

    init

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,49 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Main (main) where
 
-import HTile (someFunc)
-
+import           Data.Binary           (encodeFile)
+import           Data.ByteString.Char8 (pack)
+import           Data.Massiv.Array
+import           Data.Massiv.Array.IO
+import qualified Data.Vector.Unboxed   as V
+import           Graphics.STL
+import           HTile
+import           System.Environment    (getArgs)
 
 main :: IO ()
-main = someFunc
+main = do
+  -- TODO: proper cli
+  [inFile, outFile] <- getArgs :: IO [String]
+
+  -- Old fixed tif input mode
+  -- file <- readArray TIF filename :: IO (Image S CM.Y Word16)
+
+  -- Automatically convert colour images to greyscale using the D65
+  -- illuminant standard. If the image is already greyscale this
+  -- transformation is the identity (this is not documented in
+  -- massiv-io/color, but it is the obvious behaviour and it is what
+  -- happens)
+  file <- readImageAuto inFile :: IO (Image S (Y D65) Word16)
+  let file' = delay file
+      located = computeSource @U . locate . fmap raw $ file'
+      (Sz2 x y) = size located
+
+      x' = x - 1 -- n vertices in a row are connected by n-1 segments
+      y' = y - 1
+      -- num_facets = facets from the: up and down edges + left and
+      -- right edges + top and bottom faces
+      facets = 2 * (2 * x') + 2 * (2 * y') + 2 * (2 * (x' * y'))
+
+      sides = mkEdges located
+      surface = dropWindow $ mkTop located
+      top = toList surface
+      bottom = toList $mkBottom surface
+
+      tris = V.fromList . fmap scale . flattenTuples $ (top++bottom++sides)
+      -- tris = V.fromList . fmap scale . concat . fmap (\(a,b) -> [a,b]) $ (top++bottom++sides)
+      r = STL { header = pack "Made with hTile, by tim put."
+              , numFacets = fromIntegral facets
+              , triangles = tris
+              }
+  encodeFile outFile r
--- src/HTile.hs
+++ src/HTile.hs
@@ -6,10 +6,133 @@ Maintainer: tim put <timpu@gmail.com>
 Convert geotiffs and other images to STLs for printing or machining.
 -}
 
-module HTile
-       ( someFunc
-       ) where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies     #-}
 
+module HTile where
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+import           Data.Binary
+import qualified Data.ByteString      as B
+import           Data.Massiv.Array
+import           Data.Massiv.Array.IO hiding (V3)
+import qualified Data.Vector.Unboxed  as V
+import qualified Graphics.Color.Model as CM
+import           Graphics.STL
+import           Linear
+import           Prelude              hiding (map)
+import           System.Environment   (getArgs)
+
+flattenTuples []         = []
+flattenTuples ((a,b):xs) = a:b: flattenTuples xs
+
+-- The source files I've used are ~30m/px in the horizontal with pixel
+-- values in meters. If we scale down 1:100, then load the resulting
+-- STL in a slicer which assumes mm scale units, then we get a
+-- 1:100000 scale result, which is a good ballpark scale factor.
+scale (V4 n a b c) = rebuildNormal $ V4 n (s a) (s b) (s c)
+  where s (V3 x y z) = let m = 100 in V3 (x * (30/m)) (y*(30/m)) (z/m)
+
+-- Extract raw pixel data from colour space and pixel wrappers
+raw :: (ColorModel cs e, Components cs e ~ Word16) => Pixel cs e -> Word16
+raw = fromIntegral . toComponents . pixelColor
+
+rebuildNormal :: Triangle -> Triangle
+rebuildNormal (V4 _ a b c) = let t = c - a
+                                 u = b - a
+                             in V4 (normalize (t `cross` u)) a b c
+
+-- Annotate pixels with their locations as a function of their indices
+locate :: (Source r Ix2 Word16) => Array r Ix2 Word16 -> Array D Ix2 (V3 Float)
+locate = imap (\ (x :. y) z -> V3 (s x) (s y) (s' z))
+               where
+                 s :: Int -> Float
+                 s = fromIntegral
+                 s' :: Word16 -> Float
+                 s' = fromIntegral
+
+-- triangulate on the up diagonal |/|
+triangulation :: Stencil Ix2 (V3 Float) (Triangle, Triangle)
+triangulation = makeStencilDef 0 (Sz (2 :. 2)) (0 :. 0) $ \ get ->
+               let tl = get (0 :. 0)
+                   tr = get (1 :. 0)
+                   bl = get (0 :. 1)
+                   br = get (1 :. 1)
+               in (,) <$> (V4 <$> 0 <*> tl <*> bl <*> tr) <*> (V4 <$> 0 <*> tr <*> bl <*> br)
+{-# INLINE triangulation #-}
+
+-- Takes array of vertices to an array of pairs of triangles, each
+-- pair being the triangles which result from the square of vertices
+-- located by the top-left vertex. Note that the resulting array is
+-- smaller than the input by one in each direction.
+mkTop :: Array U Ix2 (V3 Float) -> Array DW Ix2 (Triangle, Triangle)
+mkTop = applyStencil noPadding triangulation
+
+sideStencil :: Stencil Ix1 (V3 Float) (Triangle, Triangle)
+sideStencil = makeStencilDef 0 (Sz 2) (0) $ \ get ->
+               let tl = get (0)
+                   tr = get (1)
+                   bl = project <$> tl
+                   br = project <$> tr
+               in (,) <$> (V4 <$> 0 <*> tl <*> bl <*> tr) <*> (V4 <$> 0 <*> tr <*> bl <*> br)
+  where
+    project (V3 x y _) = V3 x y 0
+{-# INLINE sideStencil #-}
+
+frontSideStencil :: Stencil Ix1 (V3 Float) (Triangle, Triangle)
+frontSideStencil = makeStencilDef 0 (Sz 2) (0) $ \ get ->
+               let tl = get (0)
+                   tr = get (1)
+                   bl = project <$> tl
+                   br = project <$> tr
+               in (,) <$> (V4 <$> 0 <*> tl <*> bl <*> tr) <*> (V4 <$> 0 <*> tr <*> bl <*> br)
+  where
+    project (V3 x y _) = V3 x y 0
+{-# INLINE frontSideStencil #-}
+
+backSideStencil :: Stencil Ix1 (V3 Float) (Triangle, Triangle)
+backSideStencil = makeStencilDef 0 (Sz 2) (0) $ \ get ->
+               let tl = get (0)
+                   tr = get (1)
+                   bl = project <$> tl
+                   br = project <$> tr
+               in (,) <$> (V4 <$> 0 <*> tl <*> tr <*> bl) <*> (V4 <$> 0 <*> tr <*> br <*> bl)
+  where
+    project (V3 x y _) = V3 x y 0
+{-# INLINE backSideStencil #-}
+
+mkFrontSides :: Manifest r Ix1 (V3 Float) =>
+             Array r Ix1 (V3 Float) ->
+             Array DW Ix1 (Triangle, Triangle)
+mkFrontSides = applyStencil noPadding frontSideStencil
+
+mkBackSides :: Manifest r Ix1 (V3 Float) =>
+            Array r Ix1 (V3 Float) ->
+            Array DW Ix1 (Triangle, Triangle)
+mkBackSides = applyStencil noPadding backSideStencil
+
+-- TODO: this might be better as (Triangle -> Triangle) applied with
+-- an fmap.
+mkBottom :: (Source r i (V4 (V3 Float), V4 (V3 Float))) =>
+         Array r i (V4 (V3 Float), V4 (V3 Float)) ->
+         Array D i (V4 (V3 Float), V4 (V3 Float))
+mkBottom = map (\(t1,t2) -> (project t1, project t2))
+    where
+      -- project flips the triangle orientation (swaps c and b) to
+      -- produce the correct normals.
+      project (V4 _ a b c) = V4 0 (project3 a) (project3 c) (project3 b)
+      project3 (V3 x y _) = V3 x y 0
+
+mkEdges :: ( Manifest (R r) Ix1 (V3 Float)
+          , OuterSlice r Ix2 (V3 Float)
+          , (InnerSlice r Ix2 (V3 Float))) =>
+            Array r Ix2 (V3 Float) ->
+          [(Triangle,Triangle)]
+mkEdges arr =
+  let (Sz2 x y) = size arr
+      f = toList . dropWindow
+      upEdge = f $ mkFrontSides (arr !> 0)
+      downEdge  = f $ mkBackSides (arr !> (y-1))
+      leftEdge  = f $ mkFrontSides (arr <! 0)
+      rightEdge  = f $ mkBackSides (arr <! (x-1))
+  in upEdge ++ downEdge ++ leftEdge ++ rightEdge
